f74c13449ea88a761e817de9a4131ae67c88fe25
Michal Limberk <michal.limberk@ylo.cz>
2020-08-12 13:05:19 +0200

    Test with y axis

--- main.lua
+++ main.lua
@@ -1,11 +1,13 @@
 function love.load()
   x = 100
+  y = 0
 end
 
 function love.update(dt)
   x = x + 100 * dt
+  y = y + 100 * dt
 end
 
 function love.draw()
-  love.graphics.rectangle("line", x, 280, 200, 200)
+  love.graphics.rectangle("line", x, y, 200, 200)
 end
